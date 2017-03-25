9426f918f01044355e321cf5579125364dc8f83b
Dricc <czalewsk@e2r9p10.42.fr>
2017-03-25 13:42:59 +0100

    add define % && directchar type

--- includes/lexer.h
+++ includes/lexer.h
@@ -6,7 +6,7 @@
 /*   By: czalewsk <czalewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/23 21:24:08 by czalewsk          #+#    #+#             */
-/*   Updated: 2017/03/24 06:21:02 by czalewsk         ###   ########.fr       */
+/*   Updated: 2017/03/25 12:16:07 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ const	char	*g_eol;
 const	char	*g_delim;
 
 enum {DIRECT, INDIRECT, LABEL, REGISTRE, INSTRUCTION, SEPARATEUR,
-	NAME, COMMENT};
+	NAME, COMMENT, DIRECTCHAR};
 
 typedef struct	s_lx
 {
