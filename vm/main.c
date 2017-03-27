9a3770c6b09aa34ca268c8f5eeebce6e8b1de3b0
lvoneduval <lduval@student.42.fr>
2017-03-27 02:17:21 +0200

    ajout de la base de la vm

---
+++ vm/main.c
@@ -0,0 +1,46 @@
+int	main(int argc, char **argv)
+{
+	init_structs();
+	/*
+	** initialise l'ensemble des structurs singletons ,
+	** utilisées tout aux long du programme.
+	*/ 
+	if (pars_param())
+		ft_error(ERR_PARAM,&free_all);
+	/*
+	 *Vérifie si le nombre de parametre est bon,
+	 *Si il est ,la foncton verifie si les différents paramètres,
+	 * sont cohérents. Et remplis les singletons adécuat(si besoin)
+	 * verifie si les fichier players spécifié existe. Return (0) si tout s'est bien passé
+	*/ 
+	if (pars_files())
+		ft_error(ERR_READ, &free_all);
+	/*
+	 * Pars les fichier de champion,  retrun (0) si tout s'est bien passé, -1 si il y a une erreur pendant la lecture ou si un champion est trop gros.
+	 */ 
+	init_map();
+	/*
+	 *
+	 * Alloue l'espace mémoire pour la partie. initialise la partie graphique si cela à était spécifié dans les parametre
+	 */ 
+	init_champ()
+	/*
+	 * place les champion sur la carte
+	 */
+	fight()
+	/*
+	 * execute la partie.
+	 *,
+	 *
+	 *
+	 *
+	 *
+	 *
+	 *
+	 */
+	free_all();
+	/*
+	 *desalloue toute la mémoire restante.
+	 */ 
+	return (EXIT_SUCCES);
+}
