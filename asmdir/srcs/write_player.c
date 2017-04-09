79e93bb65f7b53e76c2d2379a6173c2db17d977b
VynMacOs <vynmacos@VynMacOss-iMac.local>
2017-04-09 20:05:02 +0200

    Le player s'écrit maintenant correctement et est executable par la vm

--- asmdir/srcs/write_player.c
+++ asmdir/srcs/write_player.c
@@ -12,71 +12,85 @@
 
 #include "asm.h"
 
-void	set_codage(unsigned char *codage, t_lx *lx, int i)
+void	set_codage(unsigned char *codage, t_list *list_lex, t_op *op)
 {
-	if (lx->type == REGISTRE)
-		*codage = (*codage | (1 << (6 - i * 2)));
-	else if (lx->type == DIRECT)
-		*codage = (*codage | (2 << (6 - i * 2)));
-	else if (lx->type == INDIRECT)
-		*codage = (*codage | (3 << (6 - i * 2)));
-}
+	int		i;
+	t_lx	*lx;
 
-t_buf	*param_to_buffer(size_t size, t_lx *lx, t_op *op, t_list *list_label)
+	i = 0;
+	while (i < op->nb_param)
+	{
+		list_lex = get_next_field(list_lex);
+		lx = list_lex->content;
+		if (lx->type == REGISTRE)
+			*codage = (*codage | (1 << (6 - i * 2)));
+		else if (lx->type == DIRECT)
+			*codage = (*codage | (2 << (6 - i * 2)));
+		else if (lx->type == INDIRECT)
+			*codage = (*codage | (3 << (6 - i * 2)));
+		++i;
+	}
+	
+}
+void	param_to_buffer(t_buf *buffer, t_list **list_lex, t_op *op, t_list *list_label)
 {
-	t_buf		*buffer_param;
 	int			nb_octet;
 	t_label		*label;
+	t_buf 		buffer_param;
+	t_lx 		*lx;
+	int    		i;
 
-	buffer_param = (t_buf *)malloc(sizeof(t_buf));
-	buffer_param->size = 0;
-	buffer_param->data = NULL;
-	nb_octet = 0;
-	if (op->index && lx->type == DIRECT)
-		nb_octet = 2;
-	else if (lx->type == INDIRECT)
-		nb_octet = 2;
-	else if (lx->type == REGISTRE)
-		nb_octet = 1;
-	else if (lx->type == DIRECT)
-		nb_octet = 4;
-	if (lx->label)
+	i = 0;
+	buffer_param.data = NULL;
+	buffer_param.size = 0;
+	while (i < op->nb_param)
 	{
-		label = get_label(list_label, lx);
-		lx->valeur = label->octet - size;
+		nb_octet = 0;
+		*list_lex = get_next_field(*list_lex);
+		lx = (*list_lex)->content;
+		if (op->index && lx->type == DIRECT)
+			nb_octet = 2;
+		else if (lx->type == INDIRECT)
+			nb_octet = 2;
+		else if (lx->type == REGISTRE)
+			nb_octet = 1;
+		else if (lx->type == DIRECT)
+			nb_octet = 4;
+		if (lx->label)
+		{
+			label = get_label(list_label, lx);
+			lx->valeur = label->octet - (buffer->size - 1 - (op->index ? 0 : 1));
+		}
+		write_bigendian(&buffer_param, lx->valeur, nb_octet);
+		ft_printf("Writing param %s: %d => %d octet\n", lx->word, lx->valeur, nb_octet);
+		++i;
 	}
-	write_bigendian(buffer_param, lx->valeur, nb_octet);
-	ft_printf("Writing param %s: %d => %d octet\n", lx->word, lx->valeur, nb_octet);
-	return (buffer_param);
+	// Write (buffer)param to buffer
+	write_to_buffer(buffer, buffer_param.data, buffer_param.size);
+	ft_printf("size = %d\n", buffer_param.size);
 }
 
 void	write_instruction(t_buf *buffer, t_list **list_lex, t_op *op, t_list *list_label)
 {
 	int				i;
-	t_buf			*buffer_param;
+	t_buf			buffer_param;
 	unsigned char	codage;
 
 	codage = 0;	
 	i = 0;
+	buffer_param.data = NULL;
+	buffer_param.size = 0;
 	ft_printf("Writing instruction %s: %#04x\n", op->name, op->op_code);
-	while (i < op->nb_param)
-	{
-		*list_lex = get_next_field(*list_lex);
-		set_codage(&codage, (*list_lex)->content, i);
-		buffer_param = param_to_buffer(buffer->size, (*list_lex)->content, op, list_label);
-		++i;
-	}
 	// Write instruction to buffer
 	write_to_buffer(buffer, &(op->op_code), 1);
+	set_codage(&codage, *list_lex, op);
 	// Write codage to buffer
 	if (op->octet_param)
 	{
 		write_to_buffer(buffer, &codage, 1);
 		ft_printf("CODAGE %#04x\n", codage);
 	}
-	// Write (buffer)param to buffer
-	write_to_buffer(buffer, buffer_param->data, buffer_param->size);
-	ft_printf("size = %d\n", buffer_param->size);
+	param_to_buffer(buffer, list_lex, op, list_label);
 }
 
 void	write_line(t_buf *buffer, t_list **list_lex, t_list *list_label)
