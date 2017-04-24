7c9d71f0a61fdab2dc10881bb6517b1d1dadff89
lvoneduval <lduval@student.42.fr>
2017-04-24 16:16:24 +0200

    ajout du vrai initialisateur

--- vmdir/vm_data.c
+++ vmdir/vm_data.c
@@ -14,13 +14,21 @@
 
 t_vm_data	*get_data(void)
 {
+    int i;
 	static t_vm_data *data = NULL;
 
+	i = 0;
 	if (!data)
 	{
 		if (!(data = (t_vm_data *)malloc(sizeof(t_vm_data))))
 			ft_error("malloc failed in t_vm_data", NULL);
 		data->dump = -1;
+		while (i < MAX_PLAYERS + 1)
+        {
+            data->tab_champ[i] = NULL;
+            i++;
+        }
+        data->tab_proc = NULL;
 	}
 	return (data);
 }
--- vmdir/vm_main.c
+++ vmdir/vm_main.c
@@ -12,21 +12,35 @@
 
 #include "vm_header.h"
 
+void vm_print_arena(void)
+{
+    t_vm_data   *dat;
+    int         i;
+
+    dat = get_data();
+    i = 0;
+    while (i < MEM_SIZE)
+    {
+        ft_printf("%d: %2x\n", i,dat->arena[i]);
+        i++;
+    }
+}
+
 int	main(int argc, char **argv)
 {
 //	init_structs();
 	/*
 	** initialise l'ensemble des structurs singletons ,
 	** utilisées tout aux long du programme.
-	*/ 
+	*/
 	vm_pars_param(argc, argv);
 	/*
 	 * Vérifie si le nombre de parametre est bon,
 	 * Si il est ,la foncton verifie si les différents paramètres,
 	 * sont cohérents. Et remplis les singletons adécuat(si besoin)
 	 * verifie si les fichier players spécifié existe. Si ils sont correcte :Return (0) si tout s'est bien passé
-	*/  
-	//init_map();
+	*/
+	vm_init_arena();
 	/*
 	 *
 	 * Alloue l'espace mémoire pour la partie. initialise la partie graphique si cela à était spécifié dans les parametre, allloue les champion aux bon endroits:
@@ -35,11 +49,11 @@ int	main(int argc, char **argv)
 	//fight()
 	/*
 	 * execute la partie.
-	 * 
-	 */
+	 *
+
 	//vm_free_all();
 	/*
 	 * desalloue toute la mémoire restante.
-	 */ 
+	 */
 	return (EXIT_SUCCESS);
 }
