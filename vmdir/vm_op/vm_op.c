d0f79b1e6a563571baed87db35420f5e6e5e954f
lvoneduval <lduval@student.42.fr>
2017-05-01 11:31:17 +0200

    tien

---
+++ vmdir/vm_op/vm_op.c
@@ -0,0 +1,77 @@
+
+#include "vm_op.h"
+
+void    vm_ld(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	return ;
+}
+void    vm_st(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	return ;
+}
+void    vm_add(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	return ;
+}
+void    vm_sub(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	return ;
+}
+void    vm_and(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	return ;
+}
+
+	
+void    vm_or(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	return ;
+}
+
+void    vm_xor(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	return ;
+}
+
+void    vm_zjmp(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	return ;
+}
+
+void    vm_ldi(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	return ;
+}
+
+void    vm_sti(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	return ;
+}
+
+void    vm_fork(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	return ;
+}
+
+void    vm_lld(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	return ;
+}
+
+void    vm_lldi(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	return ;
+}
+
+void    vm_lfork(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	return ;
+}
+
+void    vm_aff(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	return ;
+}
+
+
+
