5519ff5fb034c2c204428b93bc2579f05eefc90d
Dricc <czalewsk@e2r9p10.42.fr>
2017-05-01 10:24:52 +0200

    add atoi big endian in vm.h

--- vmdir/vm_header.h
+++ vmdir/vm_header.h
@@ -6,7 +6,7 @@
 /*   By: lduval <lduval@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/27 19:06:07 by lduval            #+#    #+#             */
-/*   Updated: 2017/04/19 04:41:26 by lduval           ###   ########.fr       */
+/*   Updated: 2017/05/01 10:24:23 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef VM_HEADER_H
@@ -62,6 +62,7 @@ t_vm_data		*get_data();
 void			vm_free_all();
 void            vm_print_arena(void);
 
+int				ft_atoi_bigendian(unsigned char *array, int nb_octet);
 void            vm_fight(void);
 void            vm_init_arena();
 int				vm_pars_param(int nb_parm, char **tab_parm);
