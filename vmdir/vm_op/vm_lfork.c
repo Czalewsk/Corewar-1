f72b354ec6739ccc1abb2abdd868e5651b234a32
Lionel DUVAL <lduval@e2r8p9.42.fr>
2017-05-04 16:39:29 +0200

    ajout des dernieres fonctions

---
+++ vmdir/vm_op/vm_lfork.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vm_lfork.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lduval <lduval@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/04 15:40:19 by lduval            #+#    #+#             */
+/*   Updated: 2017/05/04 15:40:52 by lduval           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vm_op.h"
+
+void	vm_lfork(t_vm_data *data, t_vm_proc *proc, int pos)
+{
+	int			param;
+	t_list		*temp;
+	t_vm_proc	fork;
+
+	param = vm_get_param(data, pos + 1, 2);
+	fork.beg = (proc->beg + proc->pc + param) % MEM_SIZE;
+	fork.carry = proc->carry;
+	fork.champ = proc->champ;
+	fork.last_live = 0;
+	fork.in_proc = 0;
+	fork.prog = proc->prog;
+	fork.progsize = proc->progsize;
+	ft_memcpy(fork.registre, proc->registre, REG_SIZE * REG_NUMBER);
+	if (!(temp = ft_lstnew(&fork, sizeof(t_vm_proc))))
+		ft_error("proc list malloc failed", &vm_free_all);
+	data->nb_proc++;
+	ft_lstadd(&(data->tab_proc), temp);
+	ft_memcpy(data->arena + fork.beg, proc->prog, proc->progsize);
+}
