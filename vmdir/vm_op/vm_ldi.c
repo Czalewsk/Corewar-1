9dc5ae1ee8b275e44a07398d000c393103917fc6
Lionel DUVAL <lduval@e2r8p12.42.fr>
2017-05-06 16:42:37 +0200

    indirect function done

--- vmdir/vm_op/vm_ldi.c
+++ vmdir/vm_op/vm_ldi.c
@@ -6,7 +6,7 @@
 /*   By: lduval <lduval@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/03 09:43:54 by lduval            #+#    #+#             */
-/*   Updated: 2017/05/06 12:38:21 by lduval           ###   ########.fr       */
+/*   Updated: 2017/05/06 16:24:06 by lduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,14 @@ void	vm_ldi(t_vm_data *data, t_vm_proc *proc, int pos)
 	param[1] = vm_get_param(data, (pos + 2 + nb_octet[0]) % MEM_SIZE, nb_octet[1]);
 	param[2] = vm_get_param(data, pos + 2 + nb_octet[0] + nb_octet[1], nb_octet[2]);
 	i = 1;
-	if (ft_intisbetween_inc(param[2], 0, 15) && (((ocp >> 2) & 3) != 1 || ft_intisbetween_inc(param[0], 0, 15)))
+	if (ft_intisbetween_inc(param[2], 0, 15) && (nb_octet[0] != 1 || ft_intisbetween_inc(param[0], 0, 15)) && (nb_octet[1] != 1 || ft_intisbetween_inc(param[1], 0 , 15)))
 	{
-		while (i < 3)
+		i = 0;
+		while (i < 2)
 		{
 			if (nb_octet[i] == 1)
 				ft_memcpy(&(param[i]), proc->registre + (param[i] * REG_SIZE), REG_SIZE);
-			else if (nb_octet[i] == 2)
+			else if (((ocp >> (3 - i) * 2) & 3) == 3)
 			{
 				tmp = vm_get_param(data, pos + (param[i] % IDX_MOD) ,REG_SIZE);
 				ft_printf("\n%d", tmp);
@@ -43,6 +44,7 @@ void	vm_ldi(t_vm_data *data, t_vm_proc *proc, int pos)
 			}
 			i++;
 		}
+		ft_printf("1 : %d, 2 : %d, 3 %d\n!:%d, @:%d, #:%d i:%d" ,param[0], param[1],param[2], nb_octet[0], nb_octet[1], nb_octet[2], i);
 		i = param[0] + param[1];
 		tmp = vm_get_param(data, pos + (i % IDX_MOD), REG_SIZE);
 		ft_memcpy(proc->registre + param[2] * REG_SIZE, &tmp, REG_SIZE);
