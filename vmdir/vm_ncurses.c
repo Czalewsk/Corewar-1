73e8fbd05ef0b51a6ee6f11b76bf4057bf8716e2
xVyn <xavierfoot@live.fr>
2017-05-15 16:32:37 +0200

    Update de l'affiche avec ncurses (les inputs ne sont pas pris en charge)

--- vmdir/vm_ncurses.c
+++ vmdir/vm_ncurses.c
@@ -12,17 +12,44 @@
 
 #include "vm_header.h"
 
+void	curse_print_player_info(t_vm_champ **tab_champ, int column, int i)
+{
+	int		c;
+
+	c = 0;
+	while (tab_champ[c])
+	{
+		attron(COLOR_PAIR((ft_power(2, c))));
+		mvprintw((i = i + 2), column + 2, "Player name: %s (%d)", tab_champ[c]->name, tab_champ[c]->num);
+		mvprintw((i = i + 2), column + 2, "Alive: %d", tab_champ[c]->isalive);
+		attroff(COLOR_PAIR((ft_power(2, c))));
+		++i;
+		++c;
+	}
+}
+
 void	curse_print_globad_info(t_vm_data *data)
 {
-	int		i;
-	int		column;
+	WINDOW		*border;
+	int			i;
+	int			column;
 
 	i = 3;
-	column = 5 + (MEM_SIZE / 64) * 3;
-	mvprintw((i = i + 2), column, "nbr_cycle: %d", data->nbr_cycle);
-	mvprintw((i = i + 2), column, "cycletodie: %d", data->cycletodie);
-	mvprintw((i = i + 2), column, "nb_proc: %d", data->nb_proc);
-	mvprintw((i = i + 2), column, "winner: %s", data->winner);
+	column = (MEM_SIZE / 64) * 3;
+	border = subwin(stdscr, LINES,  COLS - column, 0, column);
+	wborder(border, '|', '|', '-', '-', '/', '\\', '\\', '/');
+	attron(A_BOLD);
+	mvprintw(1, column + ((COLS - column) / 2) - (ft_strlen("CLX Virtual Machine") / 2), "CLX Virtual Machine");
+	attroff(A_BOLD);
+	mvprintw((i = i + 2), column + 2, "dump: %d", data->dump);
+	mvprintw((i = i + 2), column + 2, "nbr_cycle: %d", data->nbr_cycle);
+	mvprintw((i = i + 2), column + 2, "cycletodie: %d", data->cycletodie);
+	mvprintw((i = i + 2), column + 2, "nb_proc: %d", data->nb_proc);
+	mvprintw((i = i + 2), column + 2, "winner: %s", data->winner);
+	curse_print_player_info(data->tab_champ, column, i + 2);
+	attron(A_DIM);
+	mvprintw(LINES - 2, column + 2, "Pls give us 250/118");
+	attroff(A_DIM);
 }
 
 void	curse_print_arena(t_vm_data *data)
@@ -38,6 +65,8 @@ void	curse_print_arena(t_vm_data *data)
 		y = 0;
 		while (y < 64 && i < MEM_SIZE)
 		{
+			if ((data->col_arena[i] & 0xf) == 0)
+					attron(A_DIM);
 			if ((data->col_arena[i] & 128))
 				attron(COLOR_PAIR(128));
 			else
@@ -47,6 +76,8 @@ void	curse_print_arena(t_vm_data *data)
 				attroff(COLOR_PAIR(128));
 			else
 				attroff(COLOR_PAIR((data->col_arena[i] & 0xf)));
+			if ((data->col_arena[i] & 0xf) == 0)
+					attroff(A_DIM);
 			++y;
 			++i;
 		}
@@ -54,12 +85,35 @@ void	curse_print_arena(t_vm_data *data)
 	}
 }
 
-void	vm_ncurses(void)
+void	curse_print_process(t_list *list_proc)
+{
+	static	t_vm_proc	*proc = NULL;
+	int					reg;
+	int					i;
+	
+	if (!proc)
+		proc = list_proc->content;
+	mvprintw(64 + 1, 1, "PC: %d, Carry: %d, Next op: %.2x (%d)",
+	proc->pc, proc->carry, proc->next_op, proc->champ);
+	i = 0;
+	while (i < REG_NUMBER)
+	{
+		ft_memcpy(&reg, proc->registre + (REG_SIZE * i), REG_SIZE);
+		mvprintw(64 + 3, 3 + (i * 11), "%d", reg);
+		i++;
+	}
+}
+
+void	vm_ncurses(void /*WINDOW *w*/)
 {
 	t_vm_data	*data;
-
+	cbreak();
+	getch();
+//	nodelay(w, TRUE);
 	data = get_data();
+	clear();
 	curse_print_arena(data);
 	curse_print_globad_info(data);
-	refresh();			/* Print it on to the real screen */
+	curse_print_process(data->tab_proc);
+	refresh();
 }
