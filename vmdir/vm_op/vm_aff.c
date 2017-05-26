8afbbacbdf950271649e157169511075a5223355
Lionel DUVAL <lduval@e2r3p6.42.fr>
2017-05-26 11:43:04 +0200

    norme sur 2 fichiers

--- vmdir/vm_op/vm_aff.c
+++ vmdir/vm_op/vm_aff.c
@@ -6,7 +6,7 @@
 /*   By: lduval <lduval@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 15:42:52 by lduval            #+#    #+#             */
-/*   Updated: 2017/05/23 07:03:22 by lduval           ###   ########.fr       */
+/*   Updated: 2017/05/26 11:42:35 by lduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 void	vm_aff(t_vm_data *data, t_vm_proc *proc, int *param, int *nb_octet)
 {
 	unsigned int	i;
-	char			s[7] = "say ";
+	char			s[2];
 
-	if (!(ft_intisbetween_inc(param[0], 0 ,15)))
+	if (!(ft_intisbetween_inc(param[0], 0, 15)))
 		return ;
 	ft_memcpy(&i, proc->registre + (param[0] * REG_SIZE), REG_SIZE);
 	data->affchar = (i % 256);
 	if (data->option & VM_OPT_S)
 	{
-		s[5] = data->affchar;
-		s[6] = '\0';
-		system(s);
+		s[0] = data->affchar;
+		s[1] = '\0';
+		ft_say(s);
 	}
 	if (data->option & VM_OPT_S || !data->option)
 		ft_printf("%c\n", data->affchar);
